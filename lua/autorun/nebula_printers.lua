2519c76218abafe8777c3e446e6b3cfa08b0a102
Wookie <bq1214@gmail.com>
2022-09-05 14:51:11 +0500

    Fixed an error with file loading

--- lua/autorun/nebula_printers.lua
+++ lua/autorun/nebula_printers.lua
@@ -4,8 +4,7 @@ AddCSLuaFile("printers/shared.lua")
 AddCSLuaFile("printers/cl_init.lua")
 
 include("printers/shared.lua")
-if CLIENT then
-    include("printers/cl_init.lua")
-else
+
+if SERVER then
     include("printers/sv_init.lua")
 end
